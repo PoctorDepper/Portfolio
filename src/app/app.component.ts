c075606f579efcee913e85d2bc8022705e82483d
NathanielSunday <61068696+NathanielSunday@users.noreply.github.com>
2025-04-16 13:01:39 -0500

    Fixed Deploy
    - Fixed ng deploy
    - Added some links so it's not just an empty page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>Under Construction</h1>
     <p>This page is under construction, please check back later.</p>
+
+    <p>
+      In the mean time, swing by my <a href="https://www.linkedin.com/in/nathille333/">LinkedIn</a>
+      or my <a href="https://github.com/NathanielSunday">GitHub</a> to see what I've been up to!
+    </p>
   `,
   styles: ``
 })
