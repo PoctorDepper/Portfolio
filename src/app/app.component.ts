26a5bf14d0ba392cd1e17e3d96356129a23fc2aa
PoctorDepper <nathille333@gmail.com>
2025-10-30 18:00:24 -0500

    Added a model for projects and cleaned up About Me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {Project} from './model/project.model';
 
 @Component({
   selector: 'app-root',
@@ -21,37 +22,40 @@ import {NgOptimizedImage} from '@angular/common';
     <section>
       <h2>About Me</h2>
       <p>
-        Hi there! My name is Nathaniel Sunday, but I usually go by Nathan, or by PoctorDepper online. I am a
-        first-generation college student from a rocky household, currently majoring in Computer and Electrical
-        Engineering with minors in Computer Science and Mathematics. I’m rather interested in microprocessors, embedded
-        systems, and general purpose robotics. In my free time, I tend to work on my ever-growing project backlog and
-        teach high school students how to design, wire, and program robots.
+        Hi there! My name is Nathaniel Sunday, but I usually go by Nathan, or by PoctorDepper online. I live with my
+        wife and two adorable, yet incredibly spoiled cats named Jinx and Luna. I am a first-generation college student
+        with a rockier childhood than most, currently majoring in Computer and Electrical Engineering with minors in
+        Computer Science and Mathematics. I’m rather interested in microprocessors, embedded systems, and general
+        purpose robotics. In my free time, I tend to work on my ever-growing project backlog and teach high school
+        students how to design, wire, and program robots every year since 2020.
       </p>
     </section>
     <section>
       <h2>Projects</h2>
-      <p>I'm working on getting a curated list here. In the mean time, visit my GitHub in my links at the bottom!</p>
+      <div class="projects">
+        @for (project of projects; track project) {
+          <img ngSrc="./{{project.image}}" alt="{{project.name}}" (click)="displayProject = project" width="100"
+               height="100">
+        }
+      </div>
+      @if (displayProject != undefined) {
+        <div class="project-viewer" animate.enter="enter-animation">
+          <img ngSrc="./{{displayProject.image}}" alt="{{displayProject.name}}" width="300" height="300">
+        </div>
+      }
     </section>
     <section>
       <h2>Skills</h2>
       <div class="skills">
-        <div>C/C++</div>
-        <div>C#</div>
-        <div>HDL</div>
-        <div>CAD</div>
-        <div>Java</div>
-        <div>Python</div>
-        <div>SQL</div>
-        <div>LaTeX</div>
-        <div>Linux systems</div>
-        <div>Microsoft Office suite</div>
-        <div>Communication</div>
-        <div>Leadership</div>
+        @for (skill of skills; track skill) {
+          <div>{{ skill }}</div>
+        }
       </div>
     </section>
     <div class="footer">
       <a href="https://www.github.com/PoctorDepper"><i class="fa fa-github"></i> Github</a>
       <a href="https://www.linkedin.com/in/nathille333/"><i class="fa fa-linkedin"></i> LinkedIn</a>
+      <a href="mailto:nathanielsunday756@gmail.com"><i class="fa fa-envelope"></i> Email</a>
     </div>
   `,
   styles: `
@@ -88,6 +92,9 @@ import {NgOptimizedImage} from '@angular/common';
       justify-content: center;
       align-items: center;
       gap: 1rem;
+      background-color: white;
+      position: sticky;
+      bottom: 0;
     }
 
     .footer a {
@@ -115,6 +122,31 @@ import {NgOptimizedImage} from '@angular/common';
       color: #666;
     }
 
+    .projects {
+
+    }
+
+    .projects img {
+
+    }
+
+    .projects-viewer {
+    }
+
+    .enter-animation {
+      animation: slide-fade 1s;
+    }
+    @keyframes slide-fade {
+      from {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+
     .skills {
       display: flex;
       /*padding: 0 2rem;*/
@@ -131,8 +163,17 @@ import {NgOptimizedImage} from '@angular/common';
       border-radius: 0.25rem;
       border: 0.125rem solid #252525;
     }
+
+    .skills div:hover {
+      color: white;
+      background-color: #252525;
+      /*border: .25rem groove #252525;*/
+    }
   `
 })
 export class AppComponent {
   title = 'Portfolio';
+  skills = ["C/C++", "C#", "HDL", "CAD", "Java", "Python", "SQL", "LaTeX", "Linux systems", "Microsoft Office suite", "Communication", "Leadership"];
+  projects:Project[] = [{name:"self", image:"selfie.jpg", description:""}];
+  displayProject:Project|undefined;
 }
---
+++ src/app/model/project.model.ts
@@ -0,0 +1,5 @@
+export class Project {
+  name: string = "";
+  image: string = "";
+  description: string = "";
+}
